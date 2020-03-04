247c2ce8269f515b18ace0c53a0d9680392b1b46
sadamdamz <sadamdamzzz@gmail.com>
2020-03-04 19:16:23 +0530

    server file

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const cors = require('cors');
+const express= require('express');
+const dotenv = require('dotenv')
+const connectDb = require('./config/db')
+
+dotenv.config({path: './config/config.env'})
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+connectDb();
+
+app.use('/api/v1/stores', require('./routes/stores'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>{
+    console.log(`server running on ${process.env.NODE_ENV} mode on ${PORT}`)
+})
