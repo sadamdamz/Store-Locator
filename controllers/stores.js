1405987a5aa4042c8d7fb75962e29db4cda51c6a
sadamdamz <sadamdamzzz@gmail.com>
2020-03-05 00:36:12 +0530

    create store request

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,3 +1,30 @@
-exports.getstores = (req, res, next) => {
-    res.send('hello');
+const Store = require('../models/Store')
+
+exports.getstores = async (req, res, next) => {
+    try {
+         const stores = await Store.find();
+         return res.status(200).json({
+             success:true,
+             count:stores.length,
+             data:stores
+         })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:'server error'})
+    }
+}
+
+exports.addstores = async (req, res, next) => {
+    try {
+        const store = await Store.create(req.body);
+        return res.status(300).json({
+            success:true,
+            data: store
+        })   
+    } catch (error) {
+        if(error.code == 11000){
+            return res.status(400).json({error:'store already exist'})
+        }
+        return res.status(500).json({error:'server error'})   
+    }
 }
